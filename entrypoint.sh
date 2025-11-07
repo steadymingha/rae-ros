aad17255a2fc05782edd7db3aaba6c23a62c4538
steadymingha <astromingha@gmail.com>
2025-11-07 15:43:35 +0000

    remove

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros environment
-echo 2 > /sys/class/pwm/pwmchip0/export && echo 1 > /sys/class/pwm/pwmchip0/export && chmod -R a+rw /sys/class/pwm/pwmchip0/pwm1/ && chmod -R a+rw /sys/class/pwm/pwmchip0/pwm2/ && chmod -R a+rw /dev/gpiochip0
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/ws/install/setup.bash"
-source "$HOME/.bashrc"
-exec "$@"
